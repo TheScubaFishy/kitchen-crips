fb4ad980258a140c7efbdcef1f37fe4d5cad27bc
TheScubaFishy <103198051+TheScubaFishy@users.noreply.github.com>
2025-03-28 08:41:54 -0500

    Bugfixes and Further Development
    - Added rudimentary map building
    - Changed all sprites to 24x24
    - Added player model placeholder
    - Fixed miscellaneous bugs
    - Began planning for future features
    - Organized files for some readability

--- Mob.gd
+++ Mob.gd
@@ -3,8 +3,7 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
-	$AnimatedSprite2D.animation = mob_types.pick_random()
+	$AnimatedSprite2D.animation = "bean_hori"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- player.gd
+++ player.gd
@@ -4,7 +4,6 @@ signal hit
 @export var speed = 100 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -17,6 +16,7 @@ func start(pos):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	@warning_ignore("shadowed_variable_base_class")
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
@@ -37,13 +37,13 @@ func _process(delta):
 	# position = position.clamp(Vector2.ZERO, screen_size)
 	
 	if velocity.x != 0:
-		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.animation = "fork_hori"
 		$AnimatedSprite2D.flip_v = false
 		# See the note below about the following boolean assignment.
-		$AnimatedSprite2D.flip_h = velocity.x > 0
+		$AnimatedSprite2D.flip_h = velocity.x < 0
 	elif velocity.y != 0:
-		$AnimatedSprite2D.animation = "walk"
-		# $AnimatedSprite2D.flip_v = velocity.y > 0
+		$AnimatedSprite2D.animation = "fork_verti"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
 		
 	move_and_slide()
 
@@ -53,3 +53,7 @@ func _process(delta):
 	# hit.emit()
 	# Must be deferred as we can't change physics properties on a physics callback.
 	# $CollisionShape2D.set_deferred("disabled", true)
+
+
+func _on_ability_2_pressed():
+	pass
